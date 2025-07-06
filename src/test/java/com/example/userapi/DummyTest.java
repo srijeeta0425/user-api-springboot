918377490855b75c2327bf24e088e4d1e76b19be
Srijeeta Purkait <srijeeta@getpostlabs.io>
2025-07-06 21:43:04 +0530

    Added unit test and updated .gitignore

---
+++ src/test/java/com/example/userapi/DummyTest.java
@@ -0,0 +1,12 @@
+package com.example.userapi;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+    @Test
+    void simpleTest() {
+        // This test will always pass
+        assert true;
+    }
+}
